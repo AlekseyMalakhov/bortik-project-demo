b491ab99b1a0e306e143dc2d831272d9f071a8d1
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-11-30 20:40:44 +0300

    publish to Heroku demo

--- components/sendCart.js
+++ components/sendCart.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config({ path: "../../project_env/.env" }); //just for dev environment
-const db = require("../db/queries");
+const path = require("path");
+const db = require(path.join(__dirname, "..", "db", "queries"));
 
 let transporter = nodemailer.createTransport({
     host: "smtp-relay.sendinblue.com",
