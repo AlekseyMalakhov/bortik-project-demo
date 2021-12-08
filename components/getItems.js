e4722acd3d27919a7a987d0d5f2d34976a7732b9
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-08 17:44:58 +0300

    publish to Heroku demo

--- components/getItems.js
+++ components/getItems.js
@@ -1,89 +1,12 @@
 const XLSX = require("xlsx");
 const path = require("path");
 const fs = require("fs");
-
-const groups = {
-    "Бумажно-гигиеническая продукция": [
-        "Бумажно-гигиеническая продукция",
-        "Бумажные полотенца",
-        "Бумажные салфетки",
-        "Прочая бумажная продукция",
-        "Туалетная бумага",
-    ],
-    "Бытовая химия": [
-        "Мыло",
-        "Освежители воздуха",
-        "Полироль для мебели",
-        "Средства для кухни",
-        "Средства для мытья пола и стен",
-        "Средства для мытья посуды",
-        "Средства для очистки стекол и зеркал",
-        "Средства для стирки",
-        "Средства для туалетов и ванных комнат",
-        "Средства для чистки ковров и обивки мебели",
-        "Средства для чистки труб",
-        "Универсальные чистящие средства",
-    ],
-    "Диспенсеры и дозаторы для общественных санузлов": ["Диспенсеры для бумажных изделий", "Дозаторы"],
-    "Канцелярские товары": ["Офисные кресла", "Художественные товары"],
-    "Одноразовая посуда": [
-        "Бумажная упаковка",
-        "Контейнеры универсальные",
-        "Крышки для стаканов",
-        "Ланч-боксы",
-        "Одноразовые пластиковые стаканы",
-        "Палочки для еды",
-        "Пластиковые контейнеры-салатники",
-        "ПЭТ-стаканы",
-        "Размешиватели",
-        "Соусники",
-        "Стаканы для горячих напитков",
-        "Столовые приборы",
-        "Суповые банки",
-        "Тарелки и миски",
-        "Трубочки",
-    ],
-    "Профессиональная химия, дезсредства": ["Дезсредства", "Профессиональная химия", "Профессиональные моющие средства Тайгета"],
-    "Спецодежда и перчатки": ["Одноразовая спецодежда", "Перчатки"],
-    "Товары для кухни, общепита и клининга": ["Термометры и гигрометры", "Товары для приготовления еды", "Украшения, сервировка для напитков и еды"],
-    "Товары для упаковки и фасовки": ["Пакеты", "Скотчи, клейкие ленты и стрейч", "Хомуты, шпагаты и сетки"],
-    "Хозяйственные товары": [
-        "Губки и мочалки хозяйственные",
-        "Лопаты, грабли, черенки",
-        "Метлы, щетки, совки для уборки",
-        "Окномойки",
-        "Протирочный материал, ветошь",
-        "Разное",
-        "Тележки для уборки",
-        "Товары для туалета",
-        "Швабры, мопы, держатели",
-    ],
-};
+const groupsFile = require("../groups");
 
 const generateListOfItems = () => {
     const buf = fs.readFileSync(path.join(__dirname, "..", "import.xlsx"));
     const list = XLSX.read(buf, { type: "buffer" });
     return list;
-};
-
-const getCategories = (arr) => {
-    const regex = new RegExp("A\\d*");
-    const arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (regex.test(arr[i].cell)) {
-            if (i !== 0) {
-                arr2.push(arr[i].value);
-            }
-        }
-    }
-    const uniq = [...new Set(arr2)];
-    const result = uniq.map((item, i) => {
-        return {
-            id: i + 1,
-            name: item,
-        };
-    });
-    return result;
 };
 
 const getNumbersOfItems = (arr) => {
@@ -104,20 +27,7 @@ const workbook = generateListOfItems();
 const data = workbook.Sheets.TDSheet;
 
 const arr = [];
-const result = {
-    items: {},
-    categories: [],
-};
-
-//
-const result1 = {
-    items: {},
-    groups: {},
-};
-//
-
 const items = [];
-
 if (data) {
     for (let x in data) {
         if (data[x].v) {
@@ -132,13 +42,11 @@ if (data) {
     const numberOfItems = getNumbersOfItems(arr);
 
     for (let i = 0; i < numberOfItems.length; i++) {
-        //for (let i = 0; i < 80; i++) {
-        //show only 80 items
         const number = numberOfItems[i];
         if (data[`B${number}`] && data[`C${number}`]) {
             const obj = {
                 id: data[`B${number}`].v + "_" + i,
-                category: data[`A${number}`].v,
+                category2: data[`A${number}`].v,
                 article: data[`B${number}`].v,
                 title: data[`C${number}`].v,
                 presence: data[`D${number}`] ? data[`D${number}`].v : 0,
@@ -152,36 +60,79 @@ if (data) {
             items.push(obj);
         }
     }
-    result.categories = getCategories(arr);
 
-    //
-    const findGroup = (categoryName) => {
-        for (let x in groups) {
-            const result = groups[x].find((name) => name === categoryName);
-            if (result) {
-                return x;
-            }
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        const catalogInfo = groupsFile.find((info) => info.category2 === item.category2);
+        if (catalogInfo) {
+            item.category1 = catalogInfo.category1;
+            item.group = catalogInfo.group;
+        } else {
+            item.category1 = "not found";
+            item.group = "not found";
         }
-        return "notFound";
-    };
-    //
-
-    for (let i = 0; i < result.categories.length; i++) {
-        const name = result.categories[i].name;
-        //
-        const group = findGroup(name);
-        if (group === "notFound") {
-            console.log(name);
-        }
-        //
-        result.items[name] = items.filter((item) => item.category === name);
-
-        //
-        // result1.groups[group][name] = items.filter((item) => item.category === name);
-        // console.log(result1);
-        //
     }
 }
+
+//console.log(items);
+
+const catalog = [];
+let id = 1;
+for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const group = item.group;
+    const category1 = item.category1;
+    const category2 = item.category2;
+
+    let groupIndex = catalog.findIndex((gr) => gr.name === group);
+    if (groupIndex === -1) {
+        const obj = {
+            id,
+            name: group,
+            items: [],
+        };
+        catalog.push(obj);
+        id++;
+    }
+    groupIndex = catalog.findIndex((gr) => gr.name === group);
+    let category1Index = -1;
+
+    if (groupIndex !== -1) {
+        category1Index = catalog[groupIndex].items.findIndex((cat1) => cat1.name === category1);
+
+        if (category1Index === -1) {
+            const obj = {
+                id,
+                name: category1,
+                items: [],
+            };
+            catalog[groupIndex].items.push(obj);
+            id++;
+        }
+        category1Index = catalog[groupIndex].items.findIndex((cat1) => cat1.name === category1);
+
+        let category2Index = -1;
+
+        if (category1Index !== -1) {
+            category2Index = catalog[groupIndex].items[category1Index].items.findIndex((cat2) => cat2.name === category2);
+
+            if (category2Index === -1) {
+                const obj = {
+                    id,
+                    name: category2,
+                    //items: [],
+                };
+                catalog[groupIndex].items[category1Index].items.push(obj);
+                id++;
+            }
+        }
+    }
+}
+
+const result = {
+    items: items,
+    catalog: catalog,
+};
 
 const getItems = (req, res) => {
     if (result.items.length !== 0) {
