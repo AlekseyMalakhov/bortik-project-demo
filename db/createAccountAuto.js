be531c90ffcf7585b7e03d7bb0ce46a8db8902d4
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-02-05 22:15:00 +0300

    publish to Heroku demo

--- db/createAccountAuto.js
+++ db/createAccountAuto.js
@@ -10,9 +10,19 @@ const createAccountAuto = async (req, res) => {
         const response1 = await pool.query(query1);
         const existingUser = response1.rows[0];
         if (!existingUser) {
+            let addressArr = "[]";
+            if (address) {
+                const addressObj = [
+                    {
+                        id: Date.now(),
+                        name: address,
+                    },
+                ];
+                addressArr = JSON.stringify(addressObj);
+            }
             const query2 = {
                 text: "INSERT INTO users (name, email, password, phone, address) VALUES($1, $2, $3, $4, $5) RETURNING *",
-                values: [name, email, "12345", phone, address],
+                values: [name, email, "12345", phone, addressArr],
             };
             const response2 = await pool.query(query2);
             const newUser = response2.rows[0];
