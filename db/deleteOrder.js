04082172f77fb989884f224d737e45cf9b3335cc
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-03-18 12:43:45 +0300

    publish to Heroku demo

--- db/deleteOrder.js
+++ db/deleteOrder.js
@@ -3,37 +3,31 @@ const pool = require("./pool");
 const deleteOrder = async (req, res) => {
     const id = req.params.id;
     try {
-        console.log(id);
-        //     const query1 = {
-        //         text: "DELETE FROM sold_items WHERE id = $1",
-        //         values: [id],
-        //     };
-        //     const response = await pool.query(query1);
-        //     if (response.rowCount !== 0) {
-        //         const query2 = {
-        //             text: "SELECT * FROM orders WHERE id = $1",
-        //             values: [order_id],
-        //         };
-        //         const response2 = await pool.query(query2);
-        //         if (response2.rowCount !== 0) {
-        //             const items = response2.rows[0].items;
-        //             const newItems = items.filter((item) => item !== parseInt(id));
-        //             const query3 = {
-        //                 text: "UPDATE orders SET items = ($1) WHERE id = ($2)",
-        //                 values: [newItems, order_id],
-        //             };
-        //             const response3 = await pool.query(query3);
-        //             if (response3.rowCount !== 0) {
-        //                 res.status(200).send(`Sold item with id = ${id} removed`);
-        //             } else {
-        //                 res.status(500).send("Error in update order");
-        //             }
-        //         } else {
-        //             res.status(404).send("Order not found");
-        //         }
-        //     } else {
-        //         res.status(404).send("Item not found");
-        //     }
+        const query1 = {
+            text: "SELECT items FROM orders WHERE id = $1",
+            values: [id],
+        };
+        const response1 = await pool.query(query1);
+        const itemsToDelete = response1.rows[0].items;
+        if (response1.rowCount !== 0) {
+            const query2 = {
+                text: "DELETE FROM sold_items WHERE id = ANY($1)",
+                values: [itemsToDelete],
+            };
+            await pool.query(query2);
+            const query3 = {
+                text: "DELETE FROM orders WHERE id = $1",
+                values: [id],
+            };
+            const response3 = await pool.query(query3);
+            if (response3.rowCount !== 0) {
+                res.status(200).send(`Order with id = ${id} removed`);
+            } else {
+                res.status(500).send("Error in delete order");
+            }
+        } else {
+            res.status(404).send("Order not found");
+        }
     } catch (error) {
         res.status(500).send(error.stack);
         console.log(error.stack);
